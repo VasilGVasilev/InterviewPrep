a8419423c1d6df492bf71940958189f1a1b4aa09
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2024-01-21 18:59:18 +0200

    cannot attach redis to express properly

--- redis/NB.md
+++ redis/NB.md
@@ -1,6 +1,6 @@
 Works inside RAM, so volatile, but not good for persistance, thus, main use case is caching.
 
-Fetching data without reddis but making a request for full data to the server with each fetch requires minimum of 150 ms per request:
+Fetching data without redis but making a request for full data to the server with each fetch requires minimum of 150 ms per request:
 
 ```sh
 const express = require("express")
@@ -23,3 +23,11 @@ app.get("/photos", async (req, res) => {
 
 app.listen(3000)
 ```
+
+Add redis to cache info and get it from redis instead of server (mind that the next thing would be to keep cache up-to-date with ex. pub/sub)
+
+To conceptualize:
+
+We run redis on a virtual machine (locally port 6379)
+We create an instance of it on our server (locally port 3000)
+When data comes into the server (3000), it is redirected via the redis client instance (within 3000) to the redis server (6379) on its own virtual machine and stored/cahced there.
--- redis/redis-node-server/server.js
+++ redis/redis-node-server/server.js
@@ -1,19 +1,45 @@
 const express = require("express")
 const axios = require("axios")
 const cors = require("cors")
+const Redis = require("redis")
+
+const redisClient = Redis.createClient({url: "redis://127.0.0.1:6379"})
+redisClient
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
+
+const DEFAULT_EXPIRATION = 3600
+
+
+
 
 const app = express()
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.get("/photos", async (req, res) => {
-    const albumId = req.query.albumId
-    const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/photos",
-        { params: {albumId}}
-    )
 
-    res.json(data)
+
+app.get("/photos", async (req, res) => {
+    const photos = await getOrSetCache("photos", async () => {
+        const { data } = await axios.get(
+            "https://jsonplaceholder.typicode.com/photos"
+        )
+        return data
+    })
+    res.json(photos)
+
+
 })
 
+function getOrSetCache(key, cb) {
+    return new Promise((resolve, reject) => {
+        redisClient.get(key, async (error, data) => {
+            if (error) return reject(error)
+            if (data != null) return resolve(JSON.parse(data))
+            let freshData = await cb()
+            redisClient.setEx(key, DEFAULT_EXPIRATION, JSON.stringify(freshData))
+            resolve(freshData)
+        })
+    })
+}
 app.listen(3000)
