f3a075879dbaed41e0352dee629543a3b06d487b
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2024-02-03 00:16:09 +0200

    liskov

---
+++ softuni-js-questions/myNotes/SOLID/test.js
@@ -0,0 +1,30 @@
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(side) {
+        // In a square, both width and height are always the same
+        super(side, side);
+    }
+}
+
+// Function expecting a Rectangle
+function printArea(rectangle) {
+    console.log(`Area: ${rectangle.getArea()}`);
+}
+
+// Using a Rectangle
+const rectangle = new Rectangle(5, 4);
+printArea(rectangle);
+
+// Using a Square (substituting Square for Rectangle)
+const square = new Square(5);
+printArea(square);
