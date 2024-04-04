fcf175a624d068b77ee6f007a5c0091a50e42aed
VasilGVasilev <vaskomeil@gmail.com>
2024-04-04 10:37:51 +0300

    prettier applied

--- softuni-js-questions/myNotes/SOLID/test.js
+++ softuni-js-questions/myNotes/SOLID/test.js
@@ -1,51 +1,51 @@
 // Abstract class representing a shape
 class Shape {
-    calculateArea() {
-        throw new Error('Not implemented');
-    }
+  calculateArea() {
+    throw new Error("Not implemented");
+  }
 }
 
 // Concrete class for rectangles
 class Rectangle extends Shape {
-    constructor(width, height) {
-        super();
-        this.width = width;
-        this.height = height;
-    }
+  constructor(width, height) {
+    super();
+    this.width = width;
+    this.height = height;
+  }
 
-    calculateArea() {
-        return this.width * this.height;
-    }
+  calculateArea() {
+    return this.width * this.height;
+  }
 }
 
 // Concrete class for circles
 class Circle extends Shape {
-    constructor(radius) {
-        super();
-        this.radius = radius;
-    }
+  constructor(radius) {
+    super();
+    this.radius = radius;
+  }
 
-    calculateArea() {
-        return Math.PI * Math.pow(this.radius, 2);
-    }
+  calculateArea() {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
 }
 
 // Function to calculate the total area of an array of shapes
 function calculateTotalArea(shapes) {
-    let totalArea = 0;
-    for (const shape of shapes) {
-        totalArea += shape.calculateArea();
-    }
-    return totalArea;
+  let totalArea = 0;
+  for (const shape of shapes) {
+    totalArea += shape.calculateArea();
+  }
+  return totalArea;
 }
 
 // Using the calculateTotalArea function without modifying existing code
 const shapes = [
-    new Rectangle(5, 4),
-    new Circle(3),
-    // You can add new shapes without modifying the calculateTotalArea function
-    // For example, adding a new Triangle class
-    // new Triangle(base, height),
+  new Rectangle(5, 4),
+  new Circle(3),
+  // You can add new shapes without modifying the calculateTotalArea function
+  // For example, adding a new Triangle class
+  // new Triangle(base, height),
 ];
 
 const totalArea = calculateTotalArea(shapes);
--- softuni-js-questions/myNotes/async/test.js
+++ softuni-js-questions/myNotes/async/test.js
@@ -1,19 +1,21 @@
-const promise = new Promise(resolve => {
-    resolve("Promise")
-})
+const promise = new Promise((resolve) => {
+  resolve("Promise");
+});
 // callback on its own will not go into macrotask queue, it will be executed immediately, putting it in a special API as setTimeout will make it go into macrotask queue
 function fetchData(cb) {
-    setTimeout(() => { cb(), 1000 });
+  setTimeout(() => {
+    cb(), 1000;
+  });
 }
 
 function processData() {
-    console.log('Callback says hello');
+  console.log("Callback says hello");
 }
 
-promise.then(res => console.log(res))
-console.log('first');
+promise.then((res) => console.log(res));
+console.log("first");
 fetchData(processData);
-console.log('last');
+console.log("last");
 
 // Output:
 // first
